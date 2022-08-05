5724b31089b7cefee72e09472237fb48cb3295a3
Nika <happynika-87@yandex.ru>
2022-08-05 18:41:11 +0600

    Lesson 1

---
+++ greeting.js
@@ -0,0 +1,9 @@
+let name = prompt('Как Вас зовут?');
+alert('Привет, ' + name);
+
+alert('Hi, ' + prompt('Your name?'))
+
+
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [123, 456, 789];
+console.log(arr1.length===arr2.length);
